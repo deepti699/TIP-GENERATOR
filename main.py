108b991c32e3aae9f4e02becc0cb5e1883d7fce5
DEEPTI BS <deeptibalgar@gmail.com>
2026-02-24 21:27:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import random
 
+a = input("Enter your name")
+
+b = input("Enter your name")
+
+c = input("Enter your name")
+
+d = input("Enter your name")
+
+random_tt = random.randint(0,4)
+
+if random_tt == 0:
+
+  print("{a} should pay the bill")
+
+elif random_tt == 1:
+
+  print("{b} should pay the bill")
+
+elif random_tt == 2:
+
+  print("{c} should apy the bill")
+
+else:
+
+  print("{d} should pay the bill")
